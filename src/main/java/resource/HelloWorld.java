86a3230720a08f6617189aaa8db94fe191859349
zebybez <zebybez@gmail.com>
2019-02-05 17:46:40 +0100

    java ee JAXRS hello world with resource

---
+++ src/main/java/resource/HelloWorld.java
@@ -0,0 +1,14 @@
+package resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("hello")
+public class HelloWorld {
+    @GET
+    @Produces("text/plain")
+    public String hello(){
+        return "hello from the resource layer";
+    }
+}
