89527afac1c5b401f0c78398c934a642c30db094
zebybez <y.riesebos@student.fontys.nl>
2019-03-11 14:10:51 +0100

    added adding posts

--- src/main/java/api/resource/PostResource.java
+++ src/main/java/api/resource/PostResource.java
@@ -1,4 +1,45 @@
 package api.resource;
 
+import business.interfaces.PostService;
+import com.sun.xml.internal.ws.client.RequestContext;
+import util.annotations.Secured;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@RequestScoped
+@Path("posts")
 public class PostResource {
+
+    PostService postService;
+
+    @Context
+    SecurityContext securityContext;
+
+    @Inject
+    public void setPostService(PostService postService){
+        this.postService = postService;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getList(){
+        return Response.ok(postService.getAll()).build();
+    }
+
+    @Secured
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response add(@HeaderParam("subreddit") String subreddit,
+                        @HeaderParam("title") String title,
+                        @HeaderParam("content") String content,
+                        @HeaderParam("isNSFW") boolean isNSFW){
+        String username = securityContext.getUserPrincipal().getName();
+        return Response.ok(postService.add(username, subreddit, title, content, isNSFW)).build();
+    }
 }
