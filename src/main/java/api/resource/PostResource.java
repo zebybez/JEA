2531ba7c8f325a5219ca81f141c3c950a96009dc
zebybez <zebybez@gmail.com>
2019-03-19 14:00:32 +0100

    added more methods to postResource

--- src/main/java/api/resource/PostResource.java
+++ src/main/java/api/resource/PostResource.java
@@ -31,6 +31,40 @@ public class PostResource {
         return Response.ok(postService.getAll()).build();
     }
 
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSingle(@PathParam("id") long id){
+        try{
+            return Response.ok(postService.getPost(id)).build();
+        } catch (Exception e){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @GET
+    @Path("sub/{subreddit}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPostsInSubreddit(@PathParam("subreddit") String subreddit){
+        try{
+            return Response.ok(postService.getPostsInSubreddit(subreddit)).build();
+        } catch (Exception e){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @Secured
+    @GET
+    @Path("user/{userName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPostsByUser(@PathParam("userName") String userName){
+        try{
+            return Response.ok(postService.getPostsByUser(userName)).build();
+        } catch (Exception e){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
     @Secured
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -40,5 +74,17 @@ public class PostResource {
                         @HeaderParam("isNSFW") boolean isNSFW){
         String username = securityContext.getUserPrincipal().getName();
         return Response.ok(postService.add(username, subreddit, title, content, isNSFW)).build();
+    }
+
+    @Secured
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") long id){
+        try{
+            return Response.ok(postService.deletePost(id)).build();
+        } catch (Exception e){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 }
