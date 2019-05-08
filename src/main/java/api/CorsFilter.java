a83f1b46fc84e84c722a3e85cc718f188224219b
zebybez <zebybez@gmail.com>
2019-05-08 19:28:41 +0200

    auth/login now returns json

---
+++ src/main/java/api/CorsFilter.java
@@ -0,0 +1,24 @@
+package api;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) {
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Headers", "email, password, token, name, subreddit, responseType");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
